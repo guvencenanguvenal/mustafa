029d056a136c3369a34492235e627b5c4bdd54ca
Güven Cenan Güvenal <guvencenan@gmail.com>
2017-02-15 20:53:42 +0300

    Control is deleted

--- src/mustafa/core/view.cr
+++ src/mustafa/core/view.cr
@@ -62,27 +62,6 @@ module Mustafa
                 end
             end
 
-            ###
-            # this macro is for initialize ECR View class with bootstrap template
-            #
-            # init "Welcomeview.ecr"
-            ###
-            macro init_bootstrap(filename)
-                def initialize (@bs_install = "<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css\" integrity=\"sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u\" crossorigin=\"anonymous\">",
-                                @bs_install_js = "<script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js\" integrity=\"sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa\" crossorigin=\"anonymous\"></script>")
-                end
-
-                ECR.def_to_s "./#{Config::VIEW_PATH.id}/#{{{filename}}}"
-            end
-
-            ###
-            # this macro load component for view
-            #
-            #
-            ###
-            macro load_custom_control(control_name)
-                property {{control_name.downcase.id}} = {{control_name.id}}.new
-            end
         end
     end
 end
